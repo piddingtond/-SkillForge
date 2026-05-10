8169d644528a6f511ffb98fc405390007dec7d44
Daniel Piddington <piddingtond@gmail.com>
2026-05-10 04:05:39 +0100

    fix: correct production domain to skill-forge.dev

--- next.config.js
+++ next.config.js
@@ -36,7 +36,7 @@ const nextConfig = {
       {
         source: '/(.*)',
         has: [{ type: 'header', key: 'x-forwarded-proto', value: 'http' }],
-        destination: 'https://skillforge.co.uk/:path*',
+        destination: 'https://skill-forge.dev/:path*',
         permanent: true,
       },
     ]
